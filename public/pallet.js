9e0a7d7ce0a6480b6f182e78d76b5e800205e8e7
ast_Sato_Kaoru <sato.k@astrolab.co.jp>
2019-07-28 21:37:24 +0900

    add spectrum.js css

---
+++ public/pallet.js
@@ -0,0 +1,13 @@
+window.onload = function () }
+$("#hideAfterPaletteSelect").spectrum({
+    showPaletteOnly: true,
+    showPalette:true,
+    hideAfterPaletteSelect:true,
+    color: 'blanchedalmond',
+    palette: [
+        ['black', 'white', 'blanchedalmond',
+        'rgb(255, 128, 0);', 'hsv 100 70 50'],
+        ['red', 'yellow', 'green', 'blue', 'violet']
+    ]
+})
+};
